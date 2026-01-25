d979cbbc99e75638e714749144ecd792d4b75907
Stephen Byron Cameron Burdine <stephenbyron31@gmail.com>
2026-01-25 16:34:59 -0600

    Updating my profile image

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './About.css';
 import theme_pattern from '../../assets/theme_pattern.svg';
-import profile_img from '../../assets/about_profile.jpg';
+import profile_img from '../../assets/about_profile.png';
 import react_logo from '../../assets/react_logo.png';
 import php_logo from '../../assets/php_logo.png';
 import swiftui_logo from '../../assets/swift.png';
